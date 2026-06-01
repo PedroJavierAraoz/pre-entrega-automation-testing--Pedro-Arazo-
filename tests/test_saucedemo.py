d832c2b424019b9d0ce701119d8bba21f1acb583
Pedro <Peterja2003@gmail.com>
2026-06-01 08:38:29 -0300

    test  login y redireccion  a  la  pagina de inventario

--- tests/test_saucedemo.py
+++ tests/test_saucedemo.py
@@ -5,5 +5,25 @@ import pytest
 def test_login(driver):
 
     login(driver,"standard_user", "secret_sauce")
-    # Verificamos que la URL cambió a la página de inventario (esto va a fallar)
-     
+    # Verificamos que la URL cambió a la página de inventario 
+    assert driver.current_url == "https://www.saucedemo.c/inventory.html", "fallo el login"
+""" 
+def test_login(driver):
+    login(driver, "standard_user", "secret_sauce")
+    # Verificamos que la URL cambió a la página de inventario 
+    assert driver.current_url == "https://www.saucedemo.com/inventory.html" """
+
+
+# def test_login_invalid_credentials(driver):
+#     login(driver, "invalid_user", "invalid_password")
+#     assert "error" in driver.page_source.lower()
+
+
+# def test_page_title_after_login(driver):
+#     login(driver, "standard_user", "secret_sauce")
+#     assert "Swag Labs" in driver.title
+
+
+# def test_inventory_page_loaded(driver):
+#     login(driver, "standard_user", "secret_sauce")
+#     assert driver.find_element("xpath", "//div[@class='inventory_list']") is not None
--- utils/helpers.py
+++ utils/helpers.py
@@ -2,6 +2,10 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import  Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 
 #dfincion  para instalar  el  driver
@@ -12,8 +16,21 @@ def get_driver():
     return driver
 
 def login(driver, user, password):
+
+    wait = WebDriverWait(driver,10)
     driver.get("https://www.saucedemo.com")
+    username=wait.until(
+        EC.presence_of_element_located((By.ID, "user-name"))
+            )
+    username.send_keys(user)
+    passwrd = wait.until(
+        EC.presence_of_element_located((By.ID,"password"))
+            )
+    passwrd.send_keys(password)
 
+    boton= wait.until(
+        EC.element_to_be_clickable((By.ID, "login-button"))
+    )
 
-
+    boton.click()
 
